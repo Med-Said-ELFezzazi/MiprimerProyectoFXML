fe8b115a225dc6aec3e5c13c30e7de2c4791402a
Mohamed said El fezzazi <144590770+Med-Said-ELFezzazi@users.noreply.github.com>
2023-09-28 08:30:41 +0200

    Avanzamiento en la configuracion de FXML, READY

--- MiPrimerProyectoFXML/src/module-info.java
+++ MiPrimerProyectoFXML/src/module-info.java
@@ -1,5 +1,11 @@
-module MiPrimerProyectoFXML {
+module PrimerProyectoFXML{
 	requires javafx.controls;
-	
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
 	opens application to javafx.graphics, javafx.fxml;
+	opens controllers to javafx.graphics, javafx.fxml;
 }
